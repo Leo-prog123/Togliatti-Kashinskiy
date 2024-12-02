703bcb87a56b2ea288322ee9cb5ea123aeb61b8e
NaN <NaN>
2024-12-02 19:37:11 +0400

    new_base

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
 
-class YellowCircles(QMainWindow):
+
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Рисование')
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.move(70, 150)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawRect(30, 30, 120, 30)
+        qp.setBrush(QColor(0, 255, 0))
+        qp.drawRect(30, 60, 120, 30)
+        qp.setBrush(QColor(0, 0, 255))
+        qp.drawRect(30, 90, 120, 30)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = YellowCircles()
+    ex = Example()
     ex.show()
     sys.exit(app.exec())
